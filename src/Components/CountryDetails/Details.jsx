a500529c47cffddde1b041e5f82c66657cc7a919
SajeedAninda <sajaninda@gmail.com>
2024-08-26 18:51:42 +0600

    Added headerImage for country details page

--- src/Components/CountryDetails/Details.jsx
+++ src/Components/CountryDetails/Details.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import topImg from "../../assets/hero-image-wr.jpg";
+import logo from "../../assets/Logo.svg";
 
 const Details = () => {
-    let countryDetails = useLoaderData();
+    let [countryDetails] = useLoaderData();
     console.log(countryDetails);
-    
+
     return (
         <div>
-            Country Name
+            <div className='upperImageDiv relative z-0'>
+                <img className='w-full' src={topImg} alt="Top Image" />
+                <img className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ' src={logo} alt="Logo" />
+            </div>
+            {countryDetails?.name?.official}
         </div>
     );
 };
