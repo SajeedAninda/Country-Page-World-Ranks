089a2d79b2cf53d01d558ec333460092c86615b4
SajeedAninda <sajaninda@gmail.com>
2024-08-26 19:30:20 +0600

    Added multiple new fields

--- src/Components/CountryDetails/Details.jsx
+++ src/Components/CountryDetails/Details.jsx
@@ -11,13 +11,13 @@ const Details = () => {
         <div>
             <div className='upperImageDiv relative z-0'>
                 <img className='w-full' src={topImg} alt="Top Image" />
-                <img className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ' src={logo} alt="Logo" />
+                <img className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={logo} alt="Logo" />
             </div>
 
             <div className='lowerDiv pb-20 min-h-[100vh] bg-[#1C1D1F] flex flex-col'>
-                <div className="innerDiv w-[70%] mx-auto h-fit rounded-xl bg-[#1C1D1F] relative p-10 border border-[#282b30] mt-[-5rem]">
+                <div className="innerDiv w-[70%] mx-auto h-fit rounded-xl bg-[#1C1D1F] relative py-10 border border-[#282b30] mt-[-5rem]">
                     <div className='countryImage w-[320px] mx-auto mt-[-5rem]'>
-                        <img className='w-full object-contain rounded-lg' src={countryDetails?.flags?.png} alt="" />
+                        <img className='w-full object-contain rounded-lg' src={countryDetails?.flags?.png} alt={`${countryDetails?.name?.common} flag`} />
                     </div>
                     <div>
                         <p className='text-[30px] font-bold text-[#D2D5DA] text-center mt-4'>
@@ -31,16 +31,72 @@ const Details = () => {
 
                     <div className='flex justify-center items-center gap-4 mt-4'>
                         <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[16px]'>
-                            <span className='pr-4'> Population</span> {countryDetails?.population?.toLocaleString()}
+                            <span className='pr-4'>Population</span> {countryDetails?.population?.toLocaleString()}
                         </div>
 
                         <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[16px]'>
                             <span className='pr-4'>Area (km<sup>2</sup>)</span> {countryDetails?.area.toLocaleString()}
                         </div>
                     </div>
+
+                    <div className='mt-8'>
+                        {countryDetails?.capital && (
+                            <div className='w-full border-t border-b border-[#46484d] flex justify-between py-6 px-10'>
+                                <div className='text-[#6C727F] text-[16px] font-semibold'>
+                                    Capital
+                                </div>
+                                <div className='text-[#D2D5DA] text-[16px] font-semibold'>
+                                    {countryDetails.capital.join(', ')}
+                                </div>
+                            </div>
+                        )}
+
+                        {countryDetails?.subregion && (
+                            <div className='w-full border-b border-[#46484d] flex justify-between py-6 px-10'>
+                                <div className='text-[#6C727F] text-[16px] font-semibold'>
+                                    Sub-Region
+                                </div>
+                                <div className='text-[#D2D5DA] text-[16px] font-semibold'>
+                                    {countryDetails.subregion}
+                                </div>
+                            </div>
+                        )}
+
+                        {countryDetails?.languages && (
+                            <div className='w-full border-b border-[#46484d] flex justify-between py-6 px-10'>
+                                <div className='text-[#6C727F] text-[16px] font-semibold'>
+                                    Language(s)
+                                </div>
+                                <div className='text-[#D2D5DA] text-[16px] font-semibold'>
+                                    {Object.values(countryDetails.languages).join(', ')}
+                                </div>
+                            </div>
+                        )}
+
+                        {countryDetails?.currencies && (
+                            <div className='w-full border-b border-[#46484d] flex justify-between py-6 px-10'>
+                                <div className='text-[#6C727F] text-[16px] font-semibold'>
+                                    Currencies
+                                </div>
+                                <div className='text-[#D2D5DA] text-[16px] font-semibold'>
+                                    {Object.values(countryDetails.currencies).map(currency => currency.name).join(', ')}
+                                </div>
+                            </div>
+                        )}
+
+                        {countryDetails?.continents && (
+                            <div className='w-full border-b border-[#46484d] flex justify-between py-6 px-10'>
+                                <div className='text-[#6C727F] text-[16px] font-semibold'>
+                                    Continent
+                                </div>
+                                <div className='text-[#D2D5DA] text-[16px] font-semibold'>
+                                    {countryDetails.continents.join(', ')}
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-            {countryDetails?.name?.official}
         </div>
     );
 };
