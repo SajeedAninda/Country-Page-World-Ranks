d70f55929f468d01319ab4e7996be40e0c1755a9
SajeedAninda <sajaninda@gmail.com>
2024-08-27 23:40:02 +0600

    Made details page responsive

--- src/Components/CountryDetails/Details.jsx
+++ src/Components/CountryDetails/Details.jsx
@@ -26,32 +26,32 @@ const Details = () => {
 
     return (
         <div>
-            <div className='upperImageDiv relative z-0'>
+            <div className='upperImageDiv relative z-0 w-full'>
                 <img className='w-full h-[50vh] object-cover' src={topImg} alt="Top Image" />
                 <img className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={logo} alt="Logo" />
             </div>
 
             <div className='lowerDiv pb-20 min-h-[100vh] bg-[#1C1D1F] flex flex-col'>
-                <div className="innerDiv lg:w-[70%] mx-auto h-fit rounded-none lg:rounded-xl bg-[#1C1D1F] relative py-10 border border-[#282b30] mt-[-5rem]">
-                    <div className='countryImage w-[320px] mx-auto mt-[-5rem]'>
+                <div className="innerDiv w-full lg:w-[70%] mx-auto h-fit rounded-none lg:rounded-xl bg-[#1C1D1F] relative py-10 border border-[#282b30] mt-[-3rem] lg:mt-[-5rem]">
+                    <div className='countryImage w-[200px] lg:w-[320px] mx-auto mt-[-5rem]'>
                         <img className='w-full object-contain rounded-lg' src={countryDetails?.flags?.png} alt={countryDetails?.name?.common} />
                     </div>
                     <div>
-                        <p className='text-[30px] font-bold text-[#D2D5DA] text-center mt-4'>
+                        <p className='text-[22px] lg:text-[30px] font-bold text-[#D2D5DA] text-center mt-4'>
                             {countryDetails?.name?.common}
                         </p>
 
-                        <p className='text-[16px] font-semibold text-[#D2D5DA] mt-1 text-center'>
+                        <p className='text-[14px] lg:text-[16px] font-semibold text-[#D2D5DA] mt-1 text-center'>
                             {countryDetails?.name?.official}
                         </p>
                     </div>
 
                     <div className='flex justify-center items-center gap-4 mt-4'>
-                        <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[16px]'>
+                        <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[12px] lg:text-[16px]'>
                             <span className='pr-4'>Population</span> {countryDetails?.population?.toLocaleString()}
                         </div>
 
-                        <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[16px]'>
+                        <div className='bg-[#282B30] px-5 py-3 rounded-xl text-[#D2D5DA] font-semibold text-[12px] lg:text-[16px]'>
                             <span className='pr-4'>Area (km<sup>2</sup>)</span> {countryDetails?.area.toLocaleString()}
                         </div>
                     </div>
@@ -105,13 +105,13 @@ const Details = () => {
 
                     {neighboringCountries.length > 0 && (
                         <div className='neighboring-countries mt-10 px-10'>
-                            <div className='text-[#6C727F] text-[16px] font-semibold'>
+                            <div className='text-[#6C727F] text-[16px] font-semibold text-center lg:text-left'>
                                 Neighbouring Countries
                             </div>
-                            <div className='flex justify-start items-center gap-6 mt-2'>
+                            <div className='flex flex-wrap justify-center lg:justify-start items-center gap-6 mt-2'>
                                 {neighboringCountries?.map((neighbor) => (
                                     <div key={neighbor?.cca3} className='neighbor-country'>
-                                        <img className='w-[80px] h-[48px] object-contain rounded-lg mb-2' src={neighbor.flags.png} alt={neighbor?.name.common} />
+                                        <img className='w-[60px] lg:w-[80px] h-[40px] lg:h-[48px] object-contain rounded-lg mb-2' src={neighbor.flags.png} alt={neighbor?.name.common} />
                                         <Link to={`/${neighbor?.name.official}`}>
                                             <p className='text-[#D2D5DA] text-[16px] text-center font-semibold hover:underline'>
                                                 {getCountryNameByCca3(neighbor.cca3)}
